52682c2d472fd1bc8e29b780e3c2f60856c65c47
Craig Fowler <craig@csf-dev.com>
2018-10-10 21:29:08 +0100

    Initial commit (basic skeleton solution)

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+  const webpackConfig = require('./webpack.config.js');
+  const testFilePattern = ['src/**/*.spec.js'];
+
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: testFilePattern.map(function(item) { return { pattern: item, watched: true }; }),
+    preprocessors: (function(){
+      let output = {};
+      testFilePattern.forEach(function(item) { output[item] = ['webpack']; });
+      return output;
+    })(),
+    webpack: webpackConfig,
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_WARN,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    phantomjsLauncher: { exitOnResourceError: true },
+    singleRun: false,
+    concurrency: Infinity
+  });
+}
