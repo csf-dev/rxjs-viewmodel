0d1cefda8804fd3c595530315f69da69f26aba7b
Craig Fowler <craig@csf-dev.com>
2018-11-01 21:25:21 +0000

    Tidy up some classes

--- src/binding/Binding.js
+++ src/binding/Binding.js
@@ -2,6 +2,15 @@
 import BindingAction from './BindingAction';
 
 export class Binding<TParams : mixed> {
-    action : BindingAction<TParams>;
-    parameters : TParams;
+    #action : BindingAction<TParams>;
+    #parameters : TParams;
+
+    get action() : BindingAction<TParams> { return this.#action; }
+    get parameters() : TParams { return this.#parameters; }
+
+    constructor(action : BindingAction<TParams>,
+                parameters : TParams) {
+        this.#action = action;
+        this.#parameters = parameters;
+    }
 }
--- src/binding/BindingAction.js
+++ src/binding/BindingAction.js
@@ -2,7 +2,16 @@
 import BindingContext from './BindingContext';
 
 export default class BindingAction<TParams : mixed> {
-    name : string;
-    handler : (ctx : BindingContext<TParams>) => void;
+    #name : string;
+    #handler : (ctx : BindingContext<TParams>) => void;
+
+    get name() : string { return this.#name; }
+    get handler() : (ctx : BindingContext<TParams>) => void { return this.#handler; }
     dependencyActions : Array<string>;
+
+    constructor(name : string, handler : (ctx : BindingContext<TParams>) => void) {
+        this.#name = name;
+        this.#handler = handler;
+        this.dependencyActions = [];
+    }
 }
