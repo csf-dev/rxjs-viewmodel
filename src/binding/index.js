2a3496c0093fd290fe4c936594a1bea619d26110
Craig Fowler <craig@csf-dev.com>
2018-11-20 20:34:32 +0000

    WIP #25 - Rework binding activator to remove phases

--- src/binding/index.js
+++ src/binding/index.js
@@ -4,6 +4,5 @@ export { default as ModelContext } from './ModelContext';
 export { default as BindingDeclaration } from './BindingDeclaration';
 export { default as LiveBinding } from './LiveBinding';
 export { default as LiveBindingsCollection } from './LiveBindingsCollection';
-export { activationPhase } from './BindingActivator';
-export type { ActivationPhase, BindingActivator } from './BindingActivator';
+export type { BindingActivator } from './BindingActivator';
 export type { ActivatableBinding } from './ActivatableBinding';
