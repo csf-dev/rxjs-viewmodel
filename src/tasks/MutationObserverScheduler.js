13587eaca9dc304529a5b66060ec7fc2f4461013
Craig Fowler <craig@csf-dev.com>
2018-11-21 19:19:23 +0000

    Resolve #40 - (trivial) add docs for the schedulers

--- src/tasks/MutationObserverScheduler.js
+++ src/tasks/MutationObserverScheduler.js
@@ -1,6 +1,21 @@
 //@flow
 import type { Scheduler } from './Scheduler';
 
+/* This scheduler should support the following browsers:
+ * * Chrome 27+
+ * * Firefox 14+
+ * * IE 11+
+ * * Opera 15+
+ * * Safari 6.1+
+ * 
+ * It uses a MutationObserver built-in JavaScript object to detect
+ * when DOM manipulation has occurred.
+ *
+ * It then uses an in-memory HTML <div> element and changes the state
+ * of one of its attributes, triggering a mutation, which resolved the
+ * promise.
+ */
+
 export default function tryGetMutationObserverScheduler() : ?Scheduler {
     if(!window || !window.document || !window['MutationObserver']) return null;
 
--- src/tasks/ReadyStateChangeScheduler.js
+++ src/tasks/ReadyStateChangeScheduler.js
@@ -2,6 +2,16 @@
 import type { Scheduler } from './Scheduler';
 import { MissingBrowserError } from '../errors/MissingBrowserError';
 
+/* This scheduler should support Internet Explorer versions 6 through
+ * 10. It uses an onreadystatechange JavaScript event upon an HTML
+ * <script> element to detect that an HTML manipulation has taken
+ * place.
+ *
+ * It functions by appending the script element to the document element,
+ * listening for the onreadystatechange event and then (when that event
+ * fires), resolving the promise and cleaning up (by removing the script).
+ */
+
 function getDocumentElement() : HTMLElement {
     if(!document.documentElement) throw new MissingBrowserError('The document must have a root element');
     return document.documentElement;
