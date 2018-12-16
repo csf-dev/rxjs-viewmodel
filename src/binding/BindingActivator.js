46a8415ad15a014cc8a438c5f0fc2fd7aa87abaf
Craig Fowler <craig@csf-dev.com>
2018-12-16 17:32:54 +0000

    Resolve #48 - Do away with early/late bindings

--- src/binding/BindingActivator.js
+++ src/binding/BindingActivator.js
@@ -5,6 +5,4 @@ export interface BindingActivator<+TParams : mixed> {
     name : string;
     activate : (ctx : BindingContext<TParams>) => ?Promise<void>;
     deactivate? : (ctx : BindingContext<TParams>) => ?Promise<void>;
-    activateAfter? : Array<string>;
-    activateEarly? : bool;
 };
